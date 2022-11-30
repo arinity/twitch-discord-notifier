283e5c135cb00342b47f4ac68033e7f7e5754134
Matt B <320696+mattwb65@users.noreply.github.com>
2022-11-30 15:20:12 -0500

    use new config class

--- src/web.ts
+++ src/web.ts
@@ -1,21 +1,24 @@
 import axios from 'axios';
+import { Config } from './config';
 import express, { Express, Request, Response } from 'express';
 import type { TokenResponse, TokenStorage } from 'interfaces/Token';
 import EventEmitter from 'node:events';
 
 export default class Web extends EventEmitter {
     private app: Express;
+    private config: Config;
 
     constructor() {
         super();
+        this.config = new Config();
         this.app = express();
         this.app.get('/', async (req: Request, res: Response) => {
             try {
                 const { data } = await axios.post<TokenResponse>(
                     'https://id.twitch.tv/oauth2/token',
                     {
-                        client_id: process.env.TWITCH_CLIENT_ID,
-                        client_secret: process.env.TWITCH_CLIENT_SECRET,
+                        client_id: this.config.twitchApplication.clientId,
+                        client_secret: this.config.twitchApplication.clientSecret,
                         code: req.query.code,
                         grant_type: 'authorization_code',
                         redirect_uri: 'http://localhost:3000',
@@ -34,7 +37,7 @@ export default class Web extends EventEmitter {
             res.send('Authorized. You may close this page.');
         })
         this.app.get('/auth', (_: Request, res: Response) => {
-            return res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${process.env.TWITCH_CLIENT_ID}&response_type=code&scope&redirect_uri=http://localhost:3000`);
+            return res.redirect(`https://id.twitch.tv/oauth2/authorize?client_id=${this.config.twitchApplication.clientId}&response_type=code&scope&redirect_uri=http://localhost:3000`);
         });
         this.app.listen(3000, () => {
             console.log('[WEB] Listening on http://127.0.0.1:3000');
